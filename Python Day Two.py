360e55bd7e0b48a7b725ab39cdc4b2d71943a3a1
mhershaft <jordan.hershaft@gmail.com>
2017-07-05 15:00:28 -0400

    Add files via upload

--- Python Day Two.py
+++ Python Day Two.py
@@ -1,18 +1,35 @@
+import random
+import string
+com_score=0
+player_score=0
+print "Welcome to Rock Paper Scissors!"
+print "Let's Play!"
 again="Yes"
 while again=="Yes" or again=="yes":
     player=raw_input("Rock Paper Or Scissors?\n")
-    import random
+    player=str.lower(player)
     computer=random.randint(0,2)
     if computer==0:
-        print "My Choice is Rock!"
+        print "My Choice is Rock!"                       #str.lower(bhruvbiv)
     elif computer==1:
         print "My Choice is Paper!"
     else:
         print "My Choice is Scissors!"
-    if computer==0 and player=="Scissors" or computer==1 and player=="Rock" or computer==2 and player=="Paper":
+    if computer==0 and player=="scissors" or computer==1 and player=="rock" or computer==2 and player=="paper":
         print "I Win!"
-    elif computer==0 and player=="Paper" or computer==1 and player=="Scissors" or computer==2 and player=="Rock":
+        com_score = com_score + 1
+    elif computer==0 and player=="paper" or computer==1 and player=="scissors" or computer==2 and player=="rock":
         print "You Win!"
+        player_score = player_score + 1
     else:
         print "Tie!"
     again=raw_input("Want to Play Again?\n")
+    if again=="No" or again=="no":
+        print "Your score was " , player_score , "."
+        print "My score was " , com_score , "."
+    if again=="No" or again=="no" and com_score > player_score:
+         print "I Win!"
+    elif again=="No" or again=="no" and com_score < player_score:
+        print "You Win!"
+    elif again=="No" or again=="no" and com_score == player_score:
+        print "We're Tied!"
